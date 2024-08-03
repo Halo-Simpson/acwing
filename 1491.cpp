fb57248fde8d7e706d2c4053b7824452e90daf69
toby <1135822892@qq.com>
2024-08-03 17:25:32 +0800

    1491 dfs

---
+++ 1491.cpp
@@ -0,0 +1,62 @@
+//剪枝版
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 15, M = 50;
+
+int n, m;
+
+bool fr[N][N];
+bool st[N];
+int buf[N];
+int idx;
+
+long long ans;
+
+//u:已经有了多少层
+void dfs(int u)
+{
+    if (u == n)
+    {
+        buf[0] = buf[n];
+        for (int i = 1; i <= n; i++)
+            if (fr[buf[i]][buf[i - 1]])
+                return;
+
+        ans++;
+        return;
+    }
+
+    for (int i = 2; i <= n; i++)
+    {
+        if (st[i] || fr[i][buf[idx]])
+            continue;
+
+        st[i] = true;
+        buf[++idx] = i;
+        dfs(u + 1);
+        --idx;
+        st[i] = false;
+    }
+}
+
+//围一圈，默认从1号开始
+int main()
+{
+    cin >> n >> m;
+    while (m--)
+    {
+        int a, b;
+        cin >> a >> b;
+        fr[a][b] = fr[b][a] = true;
+    }
+
+    //给出从1号开始的全排列
+    idx = 1;
+    buf[1] = 1;
+    dfs(1);
+
+    cout << ans;
+
+    return 0;
+}
